d27b5089e3e991f775f12a649b87cc5794198dd5
feedme24 <wireymullet@gmail.com>
2026-09-12 03:00:18 +0100

    Update stripe-backend-example.js

--- stripe-backend-example.js
+++ stripe-backend-example.js
@@ -2,11 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const Stripe = require("stripe");
 
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Feed Me backend is alive, with packages loaded!");
+  res.send("Feed Me backend is alive, Stripe initialized!");
 });
 
 const PORT = process.env.PORT || 4000;
