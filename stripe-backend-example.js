dd1ba7935bb105b34f9f9543acc3163fb549711d
feedme24 <wireymullet@gmail.com>
2026-09-12 02:33:41 +0100

    Set up basic Express server for Feed Me backend

--- stripe-backend-example.js
+++ stripe-backend-example.js
@@ -1,1 +1,10 @@
 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Feed Me backend is alive!");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Test server running on port ${PORT}`));
